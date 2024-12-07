f2d63c02d4a6625a1e4a50048a32ac9af60dbbaa
liuri <liuri@lemonit.cn>
2024-12-07 19:41:13 +0800

    增加了霸王龙模型

--- ar-painter-core/Assets/Scripts/Controller/PainterD3/PainterD3SceneController.cs
+++ ar-painter-core/Assets/Scripts/Controller/PainterD3/PainterD3SceneController.cs
@@ -1,16 +1,26 @@
 using System.Collections;
 using System.Collections.Generic;
+using UniGLTF;
 using UnityEngine;
 
 public class PainterD3SceneController : MonoBehaviour
 {
-    // Start is called before the first frame update
+    private RuntimeGltfInstance _runtimeGltfInstance;
+    private GameObject _modelGameObject;
+    
     void Start()
     {
-        
+        GltfData gltfData = new GlbFileParser(Application.streamingAssetsPath + "/Models/1.glb").Parse();
+        var loader = new ImporterContext(gltfData);
+        _runtimeGltfInstance = loader.Load();
+        _modelGameObject = _runtimeGltfInstance.gameObject;
+        _runtimeGltfInstance.EnableUpdateWhenOffscreen();
+        _runtimeGltfInstance.ShowMeshes();
+        _runtimeGltfInstance.transform.localPosition = new Vector3(0, 0, 0);
+        _runtimeGltfInstance.transform.localScale = new Vector3(1f, 1f, 1f);
+        Debug.Log("3D模型加载成功");
     }
 
-    // Update is called once per frame
     void Update()
     {
         
